d42f1c1d993d8dee7bc2b8e724707ae14ae25c0a
root <31872379@qq.com>
2020-11-18 06:43:57 +0800

    1118

---
+++ dashboard/src/main/java/com/example/dashboard/DashboardApplication.java
@@ -0,0 +1,17 @@
+package com.example.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@SpringBootApplication
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+
+}
