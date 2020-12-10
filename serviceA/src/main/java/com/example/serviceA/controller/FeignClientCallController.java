d0bcb2e4245348b7e4044ae0d61889c08a251e96
zhangct <zhangct@siyuanren.com>
2020-12-10 18:29:37 +0800

    config-server

--- serviceA/src/main/java/com/example/serviceA/controller/FeignClientCallController.java
+++ serviceA/src/main/java/com/example/serviceA/controller/FeignClientCallController.java
@@ -2,7 +2,7 @@ package com.example.serviceA.controller;
 
 
 import com.example.serviceB.common.PersonModel;
-import com.example.serviceBapi.IServiceBApi;
+import com.example.serviceBapi.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
